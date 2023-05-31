b7e05549be21343b50fe2569c124d15708446875
zachharvey97 <har16070@byui.edu>
2023-05-31 14:37:17 -0400

    Learning4

---
+++ prepare/Learning04/MathAssignment.cs
@@ -0,0 +1,16 @@
+public class MathAssignment : Assignment
+{
+    private string _textbookSection;
+    private string _problems;
+    public MathAssignment(string studentName, string topic, string textbookSection, string problems)
+        : base(studentName, topic)
+    {
+        // Here we set the MathAssignment specific variables
+        _textbookSection = textbookSection;
+        _problems = problems;
+    }
+    public string GetHomeworkList()
+    {
+        return $"Section {_textbookSection} Problems {_problems}";
+    }
+}
--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment student = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(student.GetSummary());
+
+        MathAssignment student2 = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(student2.GetSummary());
+        Console.WriteLine(student2.GetHomeworkList());
+
+        WritingAssignment student3 = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        Console.WriteLine(student3.GetSummary());
+        Console.WriteLine(student3.GetWritingInformation());
     }
 }
